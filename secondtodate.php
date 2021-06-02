8d9f6a952b8240cea12ede7869eab326b972df04
İsmail Eke <74091824+IsmailEke@users.noreply.github.com>
2021-06-02 21:11:26 +0300

    Add files via upload

---
+++ secondtodate.php
@@ -0,0 +1,15 @@
+<?php
+function secondToDateFormat (float $time = 0) : string {
+    if ($time < 0) return "Please enter a number greater than 0";
+    $year = floor($time / 31556926);
+    $time = $time - (floor($time / 31556926) * 31556926);
+    $day = floor(($time / 3600) / 24);
+    $time = $time - (($day * 24) * 3600);
+    $hour = floor($time / 3600);
+    $time = $time - ($hour * 3600);
+    $minute = floor($time / 60);
+    $second = ($time - ($minute * 60));
+    return $year . " Year / " . $day . " Day / " . $hour . " Hour / " . $minute . " Minute / " . $second . " Second";
+}
+echo secondToDateFormat(12345);
+?>
