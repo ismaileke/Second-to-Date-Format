1c3d1cfb1e8631c27fc57ccee40f351b2eb3920b
İsmail Eke <74091824+IsmailEke@users.noreply.github.com>
2021-11-14 16:38:14 +0300

    minor additions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Second-to-Date-Format
+# Usage:
+```php
+$secondtodate = new secondtodate();
+// argument type: float - int
+// data: second
+// returned value: string
+echo $secondtodate->secondToDateFormat(587);
+```
--- secondtodate.php
+++ secondtodate.php
@@ -1,4 +1,11 @@
 <?php
+
+/**
+ * @param float $time
+ * 
+ * @return string
+ */
+
 function secondToDateFormat (float $time = 0) : string {
     if ($time < 0) return "Please enter a number greater than 0";
     $year = floor($time / 31556926);
@@ -11,5 +18,4 @@ function secondToDateFormat (float $time = 0) : string {
     $second = ($time - ($minute * 60));
     return $year . " Year / " . $day . " Day / " . $hour . " Hour / " . $minute . " Minute / " . $second . " Second";
 }
-echo secondToDateFormat(12345);
 ?>
